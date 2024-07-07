e055d472847f084d7b4f57a1f4dd8d2b9393bcb9
Amit <>
2024-07-08 00:37:42 +0530

    addding new files

--- src/library/constant.js
+++ src/library/constant.js
@@ -1,4 +1,9 @@
-export const baseUrl = "http://localhost:8080/api/";
+// Load environment variables from .env file
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// Export base URL from environment variable, defaulting to a system environment variable or hardcoded value if not set
+export const baseUrl = process.env.REACT_APP_BASE_URL || process.env.BASE_URL || "http://localhost:8080/api/";
 
 export const loginUrl = "signin";
 export const RegisterUrl = "signup"
